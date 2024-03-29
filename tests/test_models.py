1db079b98c0fff8953e7671218978af11028ae78
Gilberto R Rose <gilbertorosester@gmail.com>
2024-03-29 16:10:44 -0400

    Update test_models.py
    1. Create a batch of 5 Product objects using the ProductFactory and save them to the database.
    2. Retrieve the name of the first product in the products list
    3. Count the number of occurrences of the product name in the list
    4. Retrieve products from the database that have the specified name.
    5. Assert if the count of the found products matches the expected count.
    6. Assert that each product’s name matches the expected name.

--- tests/test_models.py
+++ tests/test_models.py
@@ -149,5 +149,19 @@ class TestProductModel(unittest.TestCase):
         
         products = Product.all()
         self.assertEqual(len(products), 5) # asserting there are 5 products in collection
+    
+   
+   def test_find_by_name(self):
+        """It should Find a Product by Name"""
+        products = ProductFactory.create_batch(5)
+        for product in products:
+            product.create()
+        name = products[0].name
+        count = len([product for product in products if product.name == name])
+        found = Product.find_by_name(name)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.name, name)
+
 
 
